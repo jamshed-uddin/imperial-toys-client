0da7c8eec4ed23f8040b043663a411b32cc503be
Jami <jamsheduddin03@gmail.com>
2023-05-23 02:40:43 +0600

    gallery section added

--- src/pages/homeComponents/Banner.jsx
+++ src/pages/homeComponents/Banner.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Banner = () => {
   return (
     <div className=" flex h-screen items-center">
       <div>
-        <h1 className="lg:text-5xl text-4xl font-bold lg:w-3/4 pb-4 leading-[50px]">
+        <div className="lg:text-5xl text-4xl font-bold lg:w-3/4 pb-4 leading-[50px]">
           Embrace the Majesty of Play.Discover Majestic Toys Wonderland at The
           Imperial Toys.
-        </h1>
-        <p className=" lg:w-3/4 text-lg font-light pb-4">
+        </div>
+        <div className=" lg:w-3/4 text-lg font-light pb-4">
           Step into a World of Wonder and Imagination at The Imperial Toys.
           Where the Magic of Childhood is Unleashed, and Smiles Take Flight.
           Indulge in a Captivating Collection of Toys that Ignite Creativity,
           Nurture Curiosity, and Inspire Limitless Adventures. From Classic
           Favorites to Cutting-Edge Marvels, Our Kingdom of Play Offers Endless
           Delight for Kids of All Ages.
-        </p>
-        <button className="btn bg-pink-600 hover:bg-pink-500 border-0 text-lg">
-          Explore
-        </button>
+        </div>
+        <div>
+          <button className="btn bg-pink-600 hover:bg-pink-500 border-0 text-lg">
+            Explore
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/homeComponents/Gallery.jsx
+++ src/pages/homeComponents/Gallery.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Gallery = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div>
-      <h1>hello gallery</h1>
+      <h1 className="text-center text-4xl font-bold pb-10">
+        Have a Glimpse of Majestic Toys
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 px-16 gap-4">
+        <div data-aos="fade-right" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6 "
+            src="https://i.ibb.co/HTb2q0S/vader.jpg"
+            alt=""
+          />{" "}
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6"
+            src="https://i.ibb.co/9YXbNfd/ironman.jpg"
+            alt=""
+          />{" "}
+        </div>
+        <div data-aos="fade-left" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6"
+            src="https://i.ibb.co/TMKQM9T/blackwidow.png"
+            alt=""
+          />{" "}
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6"
+            src="https://i.ibb.co/YksWrqy/spiderman.jpg"
+            alt=""
+          />{" "}
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6"
+            src="https://i.ibb.co/9YXbNfd/ironman.jpg"
+            alt=""
+          />{" "}
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <img
+            className="border-2 rounded-2xl border-pink-600 p-6"
+            src="https://i.ibb.co/K0PrSvy/clonesoldier.jpg"
+            alt=""
+          />{" "}
+        </div>
+      </div>
     </div>
   );
 };
