883abaf934f81c965038c8a8cd2d8a17b0a0425a
Jami <jamsheduddin03@gmail.com>
2023-05-23 11:54:13 +0600

    error page added

--- src/pages/others/ErrorPage.jsx
+++ src/pages/others/ErrorPage.jsx
@@ -8,6 +8,19 @@ const ErrorPage = () => {
         <div>
           <h1 className="text-3xl font-bold">Something went wrong!</h1>
         </div>
+        <div className="py-3 text-start">
+          <h1 className="text-xl ">Try:</h1>
+          <ul className="pl-10 text-lg list-disc">
+            <li>Reloading the page.</li>
+            <li>Checking the internet connection.</li>
+            <li>Checking the URL.</li>
+          </ul>
+        </div>
+        <div>
+          <Link className="btn bg-pink-600 hover:bg-pink-500 border-0" to={"/"}>
+            Back to home
+          </Link>
+        </div>
       </div>
     </div>
   );
